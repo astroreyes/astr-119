fa5c4e26103b1617eeffbc9644aafd8c0784f4e8
astroreyes <91283471+astroreyes@users.noreply.github.com>
2021-10-06 14:56:47 -0700

    Adding astr-119-hw-2 files to this folder

---
+++ astr-119-hw-2.1/astr-119-hw-2/operators.py
@@ -0,0 +1,39 @@
+### Operators ###
+
+
+x = 9
+y = 3
+
+# Arthimitic Operators
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+x = 9.191823
+print(x//y)
+
+# Assignment operators
+x = 9
+x += 3
+print(x)
+x = 9
+x -= 3
+print(x)
+x *= 3
+print(x)
+x /= 3
+print(x)
+x **= 3
+print(x)
+
+# Comparison operators
+x = 9
+y = 3
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
